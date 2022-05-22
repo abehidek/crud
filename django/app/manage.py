400ed5d274fea0b97117d52b3b5457cd579f5db4
abehidek <hidek.abe@outlook.com>
2022-05-22 20:47:36 -0300

    Basic django api GET method

---
+++ django/app/manage.py
@@ -0,0 +1,21 @@
+#!/nix/store/ym62d0ab9agwjla3v0irlgdv3x8q971l-python3-3.10.0/bin/python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
